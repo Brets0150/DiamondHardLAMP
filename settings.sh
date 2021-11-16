2a524cd096169dcd709c2a11edac23cdc1122818
root <git@tdcme.com>
2021-11-16 07:13:56 -0800

    Removed unfinished features to prepare for v1 release. Rename global variables to identify them better quickly.

--- settings.sh
+++ settings.sh
@@ -2,31 +2,34 @@
 #######################
 ##### START CONFIG ####
 #######################
-str_adminEmail=''         # Example: admin@example.com  :: Where to send email when script is done. This will have the account login details email to it.
-str_emailRelaySever=''    # Example: mail.example.com   :: Set set a mail RELAY for your domain
-str_port=""               # Example: 22                 :: This should be the SSH port that can access the server from the Internet, so customers can SFTP here.
+# NOTE: All variables defined here have "_g_" in the variable name. This is to indicate that they are global variables.
+#######################
+str_g_adminEmail=''         # Example: admin@example.com  :: Where to send email when script is done. This will have the account login details email to it.
+str_g_emailRelaySever=''    # Example: mail.example.com   :: Set set a mail RELAY for your domain
+str_g_port=""               # Example: 22                 :: This should be the SSH port that can access the server from the Internet, so customers can SFTP here.
 #######################
 # Default Self-Signed SSL Cert Details.
 #######################
-str_country="US"
-str_state="Washington"
-str_locality="Seattle"
-str_organizationalunit="IT"
+str_g_country="US"
+str_g_state="Washington"
+str_g_locality="Seattle"
+str_g_organizationalunit="IT"
 #######################
 # URLs for packages
 #######################
-str_modsecGitUrl='https://github.com/coreruleset/coreruleset.git'
+str_g_modsecGitUrl='https://github.com/coreruleset/coreruleset.git'
+str_g_ossecGitUrl='https://github.com/ossec/ossec-hids.git'
 #######################
 # Configuration File locations and some minor settings.
 # Normally, these don't no need to change.
 #######################
-bln_letsencrypt_support=true                    # This options has to be 'true' or 'false' only.
-str_mysqlRootUser='root'                        # Mysql root user maybe 'adminer' also, depends on our install.
-str_sftpGroup='sftp'
-str_webGroup='www-data'                         # Set this to the default group apache is using.
-str_postfixConfigFile='/etc/postfix/main.cf'    # Set the postfix config file location.
-str_apache2Dir='/etc/apache2/'
-str_webMysqlToolWebDir='/usr/share/phpmyadmin/' # Other default DIRs of MySQL management tools, "/usr/share/adminer/adminer/", or "/usr/share/phpmyadmin/"
+bln_g_letsencrypt_support=true                    # This options has to be 'true' or 'false' only.
+str_g_mysqlRootUser='root'                        # Mysql root user maybe 'adminer' also, depends on our install.
+str_g_sftpGroup='sftp'
+str_g_webGroup='www-data'                         # Set this to the default group apache is using.
+str_g_postfixConfigFile='/etc/postfix/main.cf'    # Set the postfix config file location.
+str_g_apache2Dir='/etc/apache2/'
+str_g_webMysqlToolWebDir='/usr/share/phpmyadmin/' # Other default DIRs of MySQL management tools, "/usr/share/adminer/adminer/", or "/usr/share/phpmyadmin/"
 #######################
 #### END CONFIG #######
 #######################
